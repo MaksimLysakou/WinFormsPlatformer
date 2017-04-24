b0767ae095c18d9107757ca799079701d6897963
Xerotico <2008_96@mail.ru>
2017-04-24 13:55:18 +0300

    FP-3: Have made methods such as creating record, reading records from file, reading levels.

--- WinFormsPlatformer/Database.h
+++ WinFormsPlatformer/Database.h
@@ -1,12 +1,28 @@
 #include <string>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include "Level.h"
 
 using namespace std;
 
-#pragma once
-ref class Database
+class Database
 {
-// TODO: cho-nibud'
+private:
+    vector<string> records; //TODO: Make top 10 of the greatest players
+    vector<Level> levels;
+
+    Level parseLevelString(string);
+
 public:
 	Database();
+	void createRecord(string);
+    void writeRecord();
+    void readRecords();
+    void readLevels();
+    void updateRecord(int);
+    void updateLevel(int);
+    void deleteRecords(int);
+    void deleteLevel(int);
 };
 
